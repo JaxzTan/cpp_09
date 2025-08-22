0d50a74a454c4313a0b9b5d75457fdb4b8fea671
JaxzTan <chengsoo99@gmail.com>
2025-08-22 13:37:46 +0800

    feat: finish ex01

---
+++ ex01/inc/RPN.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaxztan <jaxztan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/21 13:51:25 by jaxztan           #+#    #+#             */
+/*   Updated: 2025/08/21 14:12:40 by jaxztan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <stack>
+#include <cctype>
+#include <cstdlib>
+#include <list>
+
+#define RED    "\033[0;31m"
+#define GREEN  "\033[0;32m"
+#define YELLOW "\033[33m"
+#define RESET  "\033[0m"
+
+typedef std::string str;
+
+enum Error {
+    BAD_ARG,
+    BAD_EXPRESS,
+    FAIL_COMPUTE
+};
+
+bool err_msg(Error code);
+bool isOperator(char c);
---
+++ ex01/src/main.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaxztan <jaxztan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/21 13:52:00 by jaxztan           #+#    #+#             */
+/*   Updated: 2025/08/21 14:14:38 by jaxztan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/RPN.hpp"
+
+int compute(int a, int b, char c)
+{
+    switch (c)
+    {
+        case '+': return (a + b);
+        case '-': return (a - b);
+        case '*': return (a * b);
+        case '/': 
+            if (b == 0) std::exit(err_msg(FAIL_COMPUTE));
+            return (a / b); 
+    }
+    return (0);
+}
+
+int main(int ac, char **av)
+{
+    int a, b;
+    std::list<int> list;
+
+    if (ac != 2) return err_msg(BAD_ARG);
+    str s = av[1];
+
+    // Loop through the string
+    for (size_t i=0; i < s.size(); i++)
+    {
+        // If digit, convert and put to list
+        if (isdigit(s[i]))
+            list.push_back(s[i] - '0');
+
+        // If operator, pop 2 elements (ensure have 2 elements) 
+        //  => compute => push back to list
+        else if (isOperator(s[i]))
+        {
+            if (list.size() < 2)
+                return err_msg(FAIL_COMPUTE);
+
+            b = list.back(); list.pop_back();
+            a = list.back(); list.pop_back();
+            list.push_back(compute(a, b, s[i]));
+        }
+
+        else if (s[i] == ' ')
+            continue;
+        else
+            return err_msg(BAD_EXPRESS);
+    }
+
+    // Validate final result
+    if (list.size() != 1)
+        return err_msg(BAD_EXPRESS);
+    std::cout << list.back() << "\n";
+}
